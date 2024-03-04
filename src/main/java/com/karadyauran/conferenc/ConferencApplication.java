22bbba07dd2539512030695de941826efaffd3da
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-03-04 12:07:32 +0100

    init project

---
+++ src/main/java/com/karadyauran/conferenc/ConferencApplication.java
@@ -0,0 +1,15 @@
+package com.karadyauran.conferenc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConferencApplication.class, args);
+    }
+
+}

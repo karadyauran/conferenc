c818f1dbcf52391ba0b08bfde27ebfee2853923f
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-03-04 12:24:43 +0100

    some changes in sql file

--- src/main/resources/db/migration/V1__create.sql
+++ src/main/resources/db/migration/V1__create.sql
@@ -1,7 +1,7 @@
 -- creating users table
 CREATE TABLE users
 (
-    id            SERIAL PRIMARY KEY,
+    id            UUID PRIMARY KEY,
     username      VARCHAR(255)        NOT NULL,
     email         VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255)        NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE users
 -- creating events table
 CREATE TABLE events
 (
-    id           SERIAL PRIMARY KEY,
+    id           UUID PRIMARY KEY,
     organizer_id INTEGER      NOT NULL,
     title        VARCHAR(255) NOT NULL,
     description  TEXT,
@@ -26,10 +26,10 @@ CREATE TABLE events
 -- creating bookings table
 CREATE TABLE bookings
 (
-    id         SERIAL PRIMARY KEY,
-    user_id            INTEGER NOT NULL,
-    event_id           INTEGER NOT NULL,
-    status            VARCHAR(50),
+    id                  UUID PRIMARY KEY,
+    user_id             INTEGER NOT NULL,
+    event_id            INTEGER NOT NULL,
+    status              VARCHAR(50),
     number_of_attendees INTEGER DEFAULT 1,
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (event_id) REFERENCES events (id)
@@ -38,22 +38,22 @@ CREATE TABLE bookings
 -- creating sessions table
 CREATE TABLE sessions
 (
-    id SERIAL PRIMARY KEY,
+    id         UUID PRIMARY KEY,
     event_id   INTEGER      NOT NULL,
-    title     VARCHAR(255) NOT NULL,
+    title      VARCHAR(255) NOT NULL,
     start_time TIMESTAMP    NOT NULL,
     end_time   TIMESTAMP    NOT NULL,
-    speaker   VARCHAR(255),
-    location  VARCHAR(255),
+    speaker    VARCHAR(255),
+    location   VARCHAR(255),
     FOREIGN KEY (event_id) REFERENCES events (id)
 );
 
 -- creating user_sessions table
 CREATE TABLE user_sessions
 (
-    user_id    INTEGER NOT NULL,
-    session_id INTEGER NOT NULL,
-    status    VARCHAR(50),
+    user_id    UUID NOT NULL,
+    session_id UUID NOT NULL,
+    status     VARCHAR(50),
     PRIMARY KEY (user_id, session_id),
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (session_id) REFERENCES sessions (id)
@@ -62,7 +62,7 @@ CREATE TABLE user_sessions
 -- creating event_categories table
 CREATE TABLE event_categories
 (
-    id  SERIAL PRIMARY KEY,
+    id          UUID PRIMARY KEY,
     name        VARCHAR(255) NOT NULL,
     description TEXT
 );
@@ -70,8 +70,8 @@ CREATE TABLE event_categories
 -- creating event_category_mapping table
 CREATE TABLE event_category_mapping
 (
-    event_id    INTEGER NOT NULL,
-    category_id INTEGER NOT NULL,
+    event_id    UUID NOT NULL,
+    category_id UUID NOT NULL,
     PRIMARY KEY (event_id, category_id),
     FOREIGN KEY (event_id) REFERENCES events (id),
     FOREIGN KEY (category_id) REFERENCES event_categories (id)
